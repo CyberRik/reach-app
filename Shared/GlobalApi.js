f22a8fccc0c99c9cb6e6b68467b68f86e7586b6f
CodeMatrix1 <srivallabhmanyam27@gmail.com>
2025-05-20 00:16:57 +0530

    random location genrator
    configured a random location set generator since google maps is for fetching alerts or resources except hospitals

--- Shared/GlobalApi.js
+++ Shared/GlobalApi.js
@@ -1,8 +1,14 @@
 const { default: axios } = require("axios");
 
 const getGooglePlaces=(category,radius)=>{
+    if(category==="Hospitals"){
     return axios.get('/api/search-category?'+
 'category='+category+'&radius='+radius+'&lat=38.6280278&lng=-90.1910154')}
+    else{
+        return axios.get('/api/random?'+
+'category='+category+'&radius='+radius+'&lat=38.6280278&lng=-90.1910154')
+    }
+}
 
 export default{
     getGooglePlaces
--- components/Marker.jsx
+++ components/Marker.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import { MarkerF } from "@react-google-maps/api";
 
 function Marker({ place, category }) {
+  const Schema = {
+    Medical: { src: "/Medical.png", width: 48, height: 69 },
+    Fire: { src: "/Fire.png", width: 48, height: 69 },
+    "Medical-Equipment": {
+      src: "/Medical-Equipment.png",
+      width: 30,
+      height: 30,
+    },
+    "Fire-Equipment": { src: "/Fire-Equipment.png", width: 30, height: 30 },
+    Crime: { src: "/Crime.png", width: 48, height: 69 },
+    Hospitals: { src: "/Hospitals.png", width: 30, height: 30 },
+  };
+
   return (
     <MarkerF
       position={place.geometry.location}
       icon={{
-        url: `/${category}.png`,
-        scaledSize: new window.google.maps.Size(30, 30),
+        url: Schema[category].src,
+        scaledSize: new window.google.maps.Size(
+          Schema[category].height,
+          Schema[category].width
+        ),
       }}
     />
   );
