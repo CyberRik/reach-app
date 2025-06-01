0c91523b10a4c050339ed936bfee6211c2a4da93
CyberRik <ritankarmondal123@gmail.com>
2025-06-01 19:10:19 +0530

    change

--- components/Header.jsx
+++ components/Header.jsx
@@ -46,7 +46,7 @@ function Header() {
 
   return (
     <>
-      <nav className="flex justify-between pl-4 pr-8 bg-gray-50 items-center">
+      <nav className="flex justify-between pl-4 pr-4 bg-gray-50 items-center">
         <div className="flex items-center m-3">
           <Image
             className="mx-4 cursor-pointer"
